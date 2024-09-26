ac3c28e4fa655c9652efb35e30b050acdd3e2f34
FaiyadHussain <faiyadhussain2001@gmail.com>
2024-09-26 10:18:10 +0530

    upadted timestamp

--- Back-end/models/propertyModel.js
+++ Back-end/models/propertyModel.js
@@ -1,9 +1,12 @@
 const mongoose = require("mongoose");
 
-const propertySchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  location: { type: String, required: true },
-  type: { type: String, enum: ["Mosque", "School", "land"], required: true },
-});
+const propertySchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    location: { type: String, required: true },
+    type: { type: String, enum: ["Mosque", "School", "Land"], required: true },
+  },
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("Property", propertySchema);
